984bbac5d3b45a2d681bcfdb60eecc29a6d7f586
kazbek.beteev <kazbek.beteev@accenture.com>
2021-05-27 23:23:11 +0300

    Некоторые правки по компоненту подробной информации о фильме

--- src/modules/MovieInfo/view/components/MovieInfoTrailer.tsx
+++ src/modules/MovieInfo/view/components/MovieInfoTrailer.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../../../store/hooks';
 import { isFulfilled } from '../../../../common/statusCheckers/asyncStatusCheckers';
 import { Divider, Paragraph, Subheading } from 'react-native-paper';
 import { FetchedDataChecker } from '../../../../common/statusCheckers/FetchedDataChecker';
-import { IMovieTrailerInfo } from '../../store/models';
 import { MovieInfoUtils } from '../../utils/MovieInfoUtils';
-import YoutubePlayer from 'react-native-youtube-iframe';
+import { WebView } from 'react-native-webview';
 
+/** Модель пропсов компонента. */
 interface IOwnProps {
     utils: MovieInfoUtils
 }
@@ -14,19 +14,27 @@ interface IOwnProps {
 /** Компонент трейлера. */
 export const MovieInfoTrailer: React.FC<IOwnProps> = ({ utils }) => {
         const { trailer, trailerStatus } = useAppSelector(state => state.movieInfo);
+        const [trailerKey, setTrailerKey] = useState<string>('');
 
-        /** Ключ YouTube трейлера. */
-        const getTrailerKey = (trailer: IMovieTrailerInfo | null) => {
-            if (trailer) return utils.getYouTubeKey(trailer);
-        };
+        /** Сеттим значение ключа в стейт. */
+        useEffect(() => {
+            if (trailer) {
+                const trailerKey = utils.getYouTubeKey(trailer);
+                trailerKey && setTrailerKey(trailerKey);
+            }
+        }, [trailerStatus]);
 
         return (
-            <>{getTrailerKey(trailer) && (
-                <FetchedDataChecker show={Boolean(isFulfilled(trailerStatus))}>
-                    <Divider />
-                    <Subheading>Трейлер</Subheading>
-
-                </FetchedDataChecker>)}
+            <>
+                {
+                    trailerKey && (
+                        <FetchedDataChecker show={Boolean(isFulfilled(trailerStatus))}>
+                            <Divider />
+                            <Subheading>Трейлер</Subheading>
+                            <Paragraph>{trailerKey}</Paragraph>
+                            <WebView source={{ uri: `https://www.youtube.com/embed/${trailerKey}` }} />;
+                        </FetchedDataChecker>)
+                }
             </>
         );
     }
